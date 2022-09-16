68b566e50edc4b2228bd89406f1c7788bc95bde5
Bog3008 <106614305+Bog3008@users.noreply.github.com>
2022-09-16 14:19:45 +0300

    Create 70 Climbing Stairs.cpp

---
+++ 70 Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+//tablle 
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        int * a = new int  [n];
+        //
+        a[0] = 1;
+        a[1] = 2;
+        for(int i = 2; i < n; ++i){
+            a[i] = a[i - 1]+a[i -2];
+        }
+        //
+        int answ = a[n - 1];
+        delete a;
+        return answ;
+    }
+};
